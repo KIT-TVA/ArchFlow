9cca43202c033a0759c4b4c2d5a6bebcf359d9e9
Cián Payne <uezbf@student.kit.edu>
2025-03-11 23:00:11 +0100

    Fix annoyances when a component name change is accidentally triggered

--- src/main/java/gui/controllers/interaction/EditComponentNameStrategy.java
+++ src/main/java/gui/controllers/interaction/EditComponentNameStrategy.java
@@ -2,13 +2,15 @@ package gui.controllers.interaction;
 
 import gui.Model;
 import gui.components.Component;
+import gui.views.ComponentCanvas;
 import gui.views.MainLayout;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 
 public class EditComponentNameStrategy extends BaseInteractionStrategy {
-
+    private TextField nameField;
+    private Component component;
 
     public EditComponentNameStrategy(MainLayout mainLayout, Model model, InteractionEventDispatcher eventDispatcher) {
         super(mainLayout, model, eventDispatcher);
@@ -16,25 +18,40 @@ public class EditComponentNameStrategy extends BaseInteractionStrategy {
 
     @Override
     public void componentClicked(Component component, MouseEvent event) {
+        removeTextField();
         if (event.getClickCount() > 1) {
             editComponentName(component);
         }
     }
 
-    private static void editComponentName(Component component) {
-        TextField textField = new TextField();
-        component.getContainer().getChildren().add(textField);
+    @Override
+    public void canvasClicked(ComponentCanvas canvas, MouseEvent event) {
+        removeTextField();
+    }
+
+    private void editComponentName(Component component) {
+        removeTextField();
+        nameField = new TextField();
+        this.component = component;
+        component.getContainer().getChildren().add(nameField);
         //Store the name and remove text field when enter is pressed
-        textField.setOnKeyPressed(key -> {
+        nameField.setOnKeyPressed(key -> {
             if (key.getCode().equals(KeyCode.ENTER)) {
-                String inputText = textField.getText();
-                System.out.println(inputText);
+                String inputText = nameField.getText();
                 //Remove text field
-                component.getContainer().getChildren().remove(textField);
+                component.getContainer().getChildren().remove(nameField);
                 //Set new name
                 component.setName(inputText);
             }
         });
+    }
+
+    private void removeTextField() {
+        if (nameField != null && component != null) {
+            component.getContainer().getChildren().remove(nameField);
+            nameField = null;
+            component = null;
+        }
     }
 
 }
